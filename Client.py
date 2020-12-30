fedbf281a373117516e1b671af284f3ee0e6434d
liadse <58271429+liadse@users.noreply.github.com>
2020-12-30 23:02:48 +0200

    final commit

--- Client.py
+++ Client.py
@@ -1,18 +1,141 @@
+import os
+import time
+import sys
+import tty
 from socket import *
+import threading
+import struct
+import keyboard as keyboard
+from threading import *
+# import msvcrt
+import sys, tty, termios
+from getch import getch
+from select import select
+from scapy.arch import get_if_addr
 
-serverName = 'hostname'
-serverPort = 12000
 
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.connect((serverName,serverPort))
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-sentence = input('Input lowercase sentence')
 
-clientSocket.send(sentence)
+class Client():
 
-modifiedSentence = clientSocket.recv(1024)
+    # def __init__(self, ip, port, teamName):
+    def __init__(self):
+        self.teamName = "Cyber-Funday"
+        self.tcpClientSocket = socket(AF_INET, SOCK_STREAM)
+        self.udpClientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        self.TimeToPlaybool = True
+        self.locker = threading.Lock()
+        self.connection_establish()
 
-print('From Server:',modifiedSentence)
+    def connection_establish(self):
 
-clientSocket.close()
+        clientAnswerBroadcastT = threading.Thread(target=self.clientAnswerBroadcast)
+        clientAnswerBroadcastT.start()
+        clientAnswerBroadcastT.join()
+
+    def clientAnswerBroadcast(self):
+        self.udpClientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        self.udpClientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        print("Client started, listening for offer requests...")
+        self.udpClientSocket.bind(('', 14254))
+        while True:
+            modifiedMessage, serverAddress = self.udpClientSocket.recvfrom(2048)
+            print('Received offer from %s, attempting to connect...' % str(serverAddress))
+            try:
+                magicCookie, message_type, portTcp = struct.unpack('Ibh', modifiedMessage)
+            except:
+                print(modifiedMessage)
+                continue
+
+            if magicCookie != 0xfeedbeef:
+                continue
+
+            break
+        while True:
+            try:
+                self.tcpClientSocket.connect((serverAddress[0], portTcp))
+                print(f"{bcolors.OKCYAN}TCP connect client to " + str(serverAddress[0]) + "  " + str(portTcp))
+
+                self.tcpClientSocket.send(b"Cyber-Funday\n")
+                break
+            except:
+                continue
+        self.udpClientSocket.close()
+        self.play_the_game()
+
+    def play_the_game(self):
+        try:
+            catchCharsTread = Thread(target=self.recordChars)
+            # welcome meesage
+
+            print(self.tcpClientSocket.recv(2048).decode())
+            self.TimeToPlaybool = True
+            catchCharsTread.start()
+
+            self.TimeToPlaybool = False
+            catchCharsTread.join()
+            # bye messege
+            print(self.tcpClientSocket.recv(2048).decode())
+            print("Server disconnected, listening for offer requests...")
+            self.tcpClientSocket.close()
+            return
+        except:
+            self.tcpClientSocket.close()
+            return
+
+    def sendCharsToServer(self, handle):
+        try:
+            self.tcpClientSocket.send(handle.name.encode())
+        except:
+            return
+
+    def recordChars(self):
+        numofchars = 0
+        # keyboard.on_press(self.sendCharsToServer)
+        while not self.TimeToPlaybool:
+            continue
+        self.sendCharsToServer2(self.tcpClientSocket)
+
+    def isData(self):
+        import select
+        return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+
+    def sendCharsToServer2(self, client_socket):
+        os.system("stty raw -echo")
+        previousSet = termios.tcgetattr(sys.stdin)
+        try:
+            tty.setcbreak(sys.stdin.fileno())
+
+            startTime = time.time()
+            while True:
+                # data,a,b = select([sys.stdin],[],[],0)
+                # if data:
+                input1 = sys.stdin.read(1)
+                # print(input1.encode('utf-8'))
+                sys.stdout.write(input1)
+                # client_socket.send(input1.encode('utf-8'))
+                client_socket.send(str.encode(input1))
+                if 10 < time.time() - startTime:
+                    break
+            os.system("stty -raw echo")
+
+        except:
+            print("server stop")
+        finally:
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, previousSet)
+            print("Waiting")
+            # client_socket.close()
+
+
+client = Client()
 
--- Server.py
+++ Server.py
@@ -1,22 +1,172 @@
-
+import threading
 from socket import *
-
-serverPort = 12000 #check what the ports
-
-#create UDP socket
-serverSocket = socket(AF_INET,SOCK_DGRAM)
-# bind socket to local port number 12000
-serverSocket.bind(('', serverPort))
-print("Server Started, listening on IP address 172.1.0.44")
-
-while True:
-        message, clientAddress = serverSocket.recvfrom(2048)
-        modifiedMessage = message.upper()
-        serverSocket.sendto(modifiedMessage , clientAddress)
+from threading import *
+import struct
+import time
+import random
+from scapy.arch import get_if_addr
 
 
 
+class Server(object):
+    """
+    docstring
+    """
+
+    def __init__(self):
+        # udp socket
+        self.udpServerSocket = socket(AF_INET, SOCK_DGRAM)
+        self.tcpServerSocket = socket(AF_INET, SOCK_STREAM)
+        self.tcpServerSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+        # dict of connections
+        self.clientsInGame = {}
+
+        # when clients are onnected and the game starts  - will change to true
+
+        self.firstGroup = {}
+        self.secondGroup = {}
+
+        self.locker = threading.Lock()
+        #  ___________________________________________
+
+        self.scoreFirstGroup = 0
+        self.scoreSecondGroup = 0
+        self.locker = threading.Lock()
+        self.start_Game = False
+        self.gameMode = False
+        self.InitGame()
+
+    def sendBroadcast(self):
+        waitingTime = time.time() + 10
+
+        message = struct.pack('Ibh', 0xfeedbeef, 0x2, 2044)
+        # 10 sec of a brodacast send
+        while time.time() < waitingTime:
+            #self.udpServerSocket.sendto(message, ('<broadcast>', 14254))
+            self.udpServerSocket.sendto(message, (get_if_addr('eth1'), 14254))
+            # server.sendto(message, ("255.255.255.255", 13117))
+            time.sleep(1)
+
+    #  ___________________________________________
+
+    def InitGame(self):
+
+        self.udpServerSocket.bind(('', 2044))
+        self.udpServerSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        self.tcpServerSocket.bind(('', 2044))
+        #print(f'Server started, listening on IP address {gethostbyname(gethostname())}')
+        print(f'Server started, listening on IP address {get_if_addr("eth1")}')
+        self.tcpServerSocket.listen(20)
+        self.tcpServerSocket.settimeout(0.2)
+
+        broadcastT = Thread(target=self.sendBroadcast)
+        broadcastT.start()
+
+        # while broadcastT.is_alive() and not self.gameMode:
+        while broadcastT.is_alive():
+            try:
+                with self.locker:
+                    #print(self.tcpServerSocket.getsockname())
+                    client_socket, client_info = self.tcpServerSocket.accept()
+                    print("TCP connect server")
+                    print(f"Got new connection from: {client_info}")
+                    groupName = client_socket.recv(1024).decode()
+
+                    self.clientsInGame[client_socket] = [groupName, client_info, 0, 0]
+                    self.gameMode = True
+
+            except:
+                continue
+
+        #todo check if remove
+        self.udpServerSocket.close()
+        self.tcpServerSocket.close()
+
+        self.gameMode = True
+        self.startGame()
+
+    def startGame(self):
+        if len(self.clientsInGame) <= -1:  # TODO check how many is the lower bound to start a game.
+            print("the number of clients is:" + str(self.clientsInGame))
+            print("You must To have at least 2 groups to start a game")
+            # self.client_sockets_close()
+            return
+
+        list_clients = list(self.clientsInGame.keys())
+        random.shuffle(list_clients)
+        cut = len(list_clients) // 2
+        list_1 = list_clients[:cut]
+        list_2 = list_clients[cut:]
+
+        for i in list_1:
+            self.clientsInGame[i][2] = 1
+            self.firstGroup[i] = self.clientsInGame[i]
+
+        for j in list_2:
+            self.clientsInGame[j][2] = 2
+            self.secondGroup[j] = self.clientsInGame[j]
+
+        firstGroupM = ""
+        secondGroupM = ""
+        for nameGroup1 in self.firstGroup.keys():
+            firstGroupM += self.firstGroup[nameGroup1][0]+"\n"
+
+        for nameGroup2 in self.secondGroup.keys():
+            secondGroupM += self.secondGroup[nameGroup2][0]+"\n"
+
+        for client_socket_group1 in self.firstGroup.keys():
+            m = f"Welcome to Keyboard Spamming Battle Royale.\nGroup 1:\n==\n{firstGroupM}Group 2:\n==\n{secondGroupM}\nStart pressing keys on your keyboard as fast as you can!!"
+            client_socket_group1.send(m.encode())
+            game_thread1 = Thread(target=self.game_thread, args=(client_socket_group1,))
+            self.clientsInGame[client_socket_group1][3] = game_thread1
+            game_thread1.start()
+
+        for client_socket_group2 in self.secondGroup.keys():
+            m = f"Welcome to Keyboard Spamming Battle Royale.\nGroup 1:\n==\n{firstGroupM}Group 2:\n==\n{secondGroupM}\nStart pressing keys on your keyboard as fast as you can!!"
+            client_socket_group2.send(m.encode())
+            game_thread2 = Thread(target=self.game_thread, args=(client_socket_group2,))
+            self.clientsInGame[client_socket_group2][3] = game_thread2
+            game_thread2.start()
+
+        for t in self.clientsInGame.keys():
+            self.clientsInGame[t][3].join()
+
+        self.start_Game = False
+        if self.scoreFirstGroup > self.scoreSecondGroup:
+            win = "Group 1 wins!"
+            group_name = firstGroupM
+        elif self.scoreFirstGroup < self.scoreSecondGroup:
+            win = "Group 2 wins!"
+            group_name = secondGroupM
+        else:
+            win = "That's a draw!"
+            group_name = "Everybody wins!!!"
+
+        for client_socket_group1 in self.firstGroup.keys():
+            m = f"Game over!\nGroup 1 typed in {self.scoreFirstGroup} characters. Group 2 typed in {self.scoreSecondGroup} characters. \n{win} \n\n Congratulations to the winners:\n==\n{group_name}"
+            client_socket_group1.send(m.encode())
+            client_socket_group1.close()
+
+        for client_socket_group2 in self.secondGroup.keys():
+            m = f"Game over!\nGroup 1 typed in {self.scoreFirstGroup} characters. Group 2 typed in {self.scoreSecondGroup} characters. \n{win} \n\n Congratulations to the winners:\n==\n{group_name}"
+            client_socket_group2.send(m.encode())
+            client_socket_group2.close()
 
 
+        print("Game over, sending out offer requests...")
 
-# def handle_client(client_socket):
+    def game_thread(self, clientSocket):
+        start_game_time = time.time()
+        self.start_Game = True
+        countChars = 0
+        while time.time() - start_game_time < 10:
+            data = clientSocket.recv(2048).decode()
+            countChars += 1
+        if self.clientsInGame[clientSocket][2] == 1:
+            self.scoreFirstGroup += countChars
+        else:
+            self.scoreSecondGroup += countChars
+
+
+server = Server()
